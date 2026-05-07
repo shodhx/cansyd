2930f0396eef0b0359ef7486db9a0722d0b896d6
abhiprd2000 <8292aniarc@gmail.com>
2026-05-07 18:21:01 +0530

    feat: added accuracy and wilcoxon tests for the paper

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,4 +1,7 @@
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from scipy.stats import wilcoxon
 
 def report_classification(y_true, name="dataset"):
     acc = accuracy_score(y_true, y_pred)
@@ -6,3 +9,18 @@ def report_classification(y_true, name="dataset"):
     
     print(f"{name} -> acc: {acc:.4f} | f1: {f1:.4f}")
     return acc, f1
+
+def Check_Acc(true_y, pred_y):
+    a = accuracy_score(true_y, pred_y)
+    f1 = f1_score(true_y, pred_y, average='weighted')
+    print(a)
+    print(f1)
+    return a, f1
+
+def do_wilcoxon(base_ate, new_ate):
+    if np.array_equal(base_ate, new_ate):
+        return 1.0
+        
+    stat, p = wilcoxon(base_ate, new_ate)
+    print(p)
+    return p
