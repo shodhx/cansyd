4859c366a825d503a26fc08642f971dcf702471a
abhiprd2000 <8292aniarc@gmail.com>
2026-05-13 11:06:29 +0530

    feat: final setup patch

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cnsd',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow>=2.15.0',
+        'numpy>=1.24.3',
+        'scipy>=1.11.3',
+        'pandas>=2.0.3',
+        'scikit-learn>=1.3.0',
+        'wfdb>=4.1.0',
+        'dowhy>=0.11',
+    ],
+    python_requires='>=3.8',
+)
