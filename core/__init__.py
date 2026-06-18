0bb0e6b05a146a70a7beb6445d9e1233817008a0
abhiprd2000 <8292aniarc@gmail.com>
2026-06-18 06:31:32 +0530

    fix core/__init__.py stale export

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,7 @@
 from .architecture import build_cnn
 from .causal import analyze_causal
-from .rules import BearingRule
+from .rules import PhysicsRuleEngine, rule_engine
 from .pipeline import CNSDPipeline
 
-__all__ = ["build_cnn", "analyze_causal", "BearingRule", "CNSDPipeline"]
+__all__ = ["build_cnn", "analyze_causal", "PhysicsRuleEngine", "rule_engine",
+           "CNSDPipeline"]
